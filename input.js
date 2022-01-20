20d8aa83a14400bfd22234340a6f0a8e9b5f1fcc
Conor Mancone <cmancone@gmail.com>
2022-01-20 16:23:42 -0500

    Input processing

--- input.js
+++ input.js
@@ -1,6 +1,25 @@
 const core = require('@actions/core');
 const assert = require('assert')
 
+const stringInputs = {
+    'accessId': 'access-id',
+    'accessType': 'access-type',
+    'apiUrl': 'api-url',
+    'producerForAwsAccess': 'producer-for-aws-access',
+}
+
+const boolInputs = {
+    'exportSecretsToOutputs': 'export-secrets-to-outputs',
+    'exportSecretsToEnvironment': 'export-secrets-to-environment',
+}
+
+const dictInputs = {
+    'staticSecrets': 'static-secrets',
+    'dynamicSecrets': 'dynamic-secrets',
+}
+
+const allowedAccessTypes = ['jwt', 'aws_iam']
+
 const fetchAndValidateInput = () => {
     const params = {
         accessId: core.getInput('access-id'),
@@ -12,9 +31,34 @@ const fetchAndValidateInput = () => {
         exportSecretsToOutputs: core.getInput('export-secrets-to-outputs'),
         exportSecretsToEnvironment: core.getInput('export-secrets-to-environment'),
     }
+    // our only required parameter
     assert(params['accessId'], 'You must provide the access id for your auth method via the access-id input')
-    return {
+
+    // check for string types
+    for (const [paramKey, inputId] of Object.entries(stringInputs)) {
+        if (typeof params[paramKey] !== 'string') {
+            throw new Error(`Input '${inputId}' should be a string`)
+        }
     }
+    // check for bool types
+    for (const [paramKey, inputId] of Object.entries(boolInputs)) {
+        if(typeof params[paramKey] !== 'boolean') {
+            throw new Error(`Input '${inputId}' should be a boolean`)
+        }
+    }
+    // check for dict types
+    for (const [paramKey, inputId] of Object.entries(dictInputs)) {
+        if (params[paramKey].constructor != Object) {
+            throw new Error(`Input '${inputId}' should be a dictionary with the secret path as a key and the output name as the value`)
+        }
+    }
+    // check access types
+    if (!allowedAccessTypes.includes(params['accessType'].toLowerCase())) {
+        throw new Error('access-type must be one of ' + allowedAccessTypes.join(', '))
+    }
+    params['accessType'] = params['accessType'].toLowerCase()
+
+    return params
 }
 
 exports.fetchAndValidateInput = fetchAndValidateInput
---
+++ input.test.js
@@ -0,0 +1,45 @@
+jest.mock('@actions/core');
+
+core = require('@actions/core');
+input = require('./input')
+
+test('Input is all good', () => {
+    core.getInput = jest.fn()
+    core.getInput.mockReturnValueOnce('p-asdf')
+    core.getInput.mockReturnValueOnce('JWT')
+    core.getInput.mockReturnValueOnce('https://api.akeyless.io')
+    core.getInput.mockReturnValueOnce('/path/to/aws/producer')
+    core.getInput.mockReturnValueOnce({'/some/static/secret': 'secret_key'})
+    core.getInput.mockReturnValueOnce({'/some/dynamic/secret': 'other_key'})
+    core.getInput.mockReturnValueOnce(true)
+    core.getInput.mockReturnValueOnce(true)
+    params = input.fetchAndValidateInput()
+    expect(params).toEqual({
+        'accessId': 'p-asdf',
+        'accessType': 'jwt',
+        'apiUrl': 'https://api.akeyless.io',
+        'producerForAwsAccess': '/path/to/aws/producer',
+        'staticSecrets': {'/some/static/secret': 'secret_key'},
+        'dynamicSecrets': {'/some/dynamic/secret': 'other_key'},
+        'exportSecretsToOutputs': true,
+        'exportSecretsToEnvironment': true,
+    })
+    expect(core.getInput.mock.calls).toEqual([
+        ['access-id'],
+        ['access-type'],
+        ['api-url'],
+        ['producer-for-aws-access'],
+        ['static-secrets'],
+        ['dynamic-secrets'],
+        ['export-secrets-to-outputs'],
+        ['export-secrets-to-environment'],
+    ]);
+});
+
+test('check string', () => {
+    core.getInput = jest.fn()
+    core.getInput.mockReturnValueOnce('p-asdf')
+    core.getInput.mockReturnValueOnce(343)
+    core.getInput.mockReturnValue('sup')
+    expect(() => {input.fetchAndValidateInput()}).toThrow("Input 'access-type' should be a string")
+})
