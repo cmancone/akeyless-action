263c18074fccb771178d89d321f061a2c8ac8549
Conor Mancone <cmancone@gmail.com>
2022-01-23 14:25:27 -0500

    Of course the tests are fine...

--- secrets.js
+++ secrets.js
@@ -64,15 +64,19 @@ async function exportStaticSecrets(akeylessToken, staticSecrets, apiUrl, exportS
     const api = akeylessApi.api(apiUrl);
     let toAwait = []
     for (const [akeylessPath, variableName] of Object.entries(staticSecrets)) {
-        toAwait.push(getStaticSecret(
+        let promise = getStaticSecret(
             api,
             akeylessPath,
             variableName,
             akeylessToken,
             exportSecretsToOutputs,
             exportSecretsToEnvironment,
-        ));
+        );
+        core.debug('pushing to list:');
+        core.debug(promise);
+        toAwait.push(promise);
     }
+    core.debug('list:');
     core.debug(toAwait);
     return toAwait;
 }
