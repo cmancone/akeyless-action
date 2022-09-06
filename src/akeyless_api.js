7aeae3914a74f8ac531163691c48ebf86cd39720
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2022-09-06 12:56:02 -0400

    Update akeyless_api.js
    Handled linting rules.

--- src/akeyless_api.js
+++ src/akeyless_api.js
@@ -1,7 +1,8 @@
 const akeyless = require('akeyless');
+const { default: K8SAuthsConfigLastChange } = require('akeyless/dist/model/K8SAuthsConfigLastChange');
 
 function api(url) {
-  let client = new akeyless.ApiClient();
+  K8SAuthsConfigLastChange client = new akeyless.ApiClient();
   client.basePath = url;
   return new akeyless.V2Api(client);
 }
