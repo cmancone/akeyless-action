feb9f43508638fbe8751167d220f2d4a05c924d2
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2022-09-06 12:08:21 -0400

    Format code

--- src/akeyless_api.js
+++ src/akeyless_api.js
@@ -1,9 +1,9 @@
 const akeyless = require('akeyless');
 
 function api(url) {
-    let client = new akeyless.ApiClient();
-    client.basePath = url;
-    return new akeyless.V2Api(client);
+  let client = new akeyless.ApiClient();
+  client.basePath = url;
+  return new akeyless.V2Api(client);
 }
 
-exports.api = api
+exports.api = api;
--- src/aws_access.js
+++ src/aws_access.js
@@ -3,29 +3,34 @@ const akeylessApi = require('./akeyless_api');
 const akeyless = require('akeyless');
 
 async function awsLogin(akeylessToken, producerForAwsAccess, apiUrl) {
-    let api = akeylessApi.api(apiUrl);
-    return new Promise((resolve, reject) => {
-        return api.getDynamicSecretValue(akeyless.GetDynamicSecretValue.constructFromObject({
-            'token': akeylessToken,
-            'name': producerForAwsAccess,
-        })).then(awsCredentials => {
-            const accessKeyId = awsCredentials['access_key_id'];
-            const secretAccessKey = awsCredentials['secret_access_key'];
-            const sessionToken = awsCredentials['security_token'];
-
-            core.setSecret(accessKeyId);
-            core.exportVariable('AWS_ACCESS_KEY_ID', accessKeyId);
-            core.setSecret(secretAccessKey);
-            core.exportVariable('AWS_SECRET_ACCESS_KEY', secretAccessKey);
-            if (sessionToken) {
-                core.setSecret(sessionToken);
-                core.exportVariable('AWS_SESSION_TOKEN', sessionToken);
-            }
-            resolve();
-        }).catch(error => {
-            reject(error);
+  let api = akeylessApi.api(apiUrl);
+  return new Promise((resolve, reject) => {
+    return api
+      .getDynamicSecretValue(
+        akeyless.GetDynamicSecretValue.constructFromObject({
+          token: akeylessToken,
+          name: producerForAwsAccess
         })
-    });
+      )
+      .then(awsCredentials => {
+        const accessKeyId = awsCredentials['access_key_id'];
+        const secretAccessKey = awsCredentials['secret_access_key'];
+        const sessionToken = awsCredentials['security_token'];
+
+        core.setSecret(accessKeyId);
+        core.exportVariable('AWS_ACCESS_KEY_ID', accessKeyId);
+        core.setSecret(secretAccessKey);
+        core.exportVariable('AWS_SECRET_ACCESS_KEY', secretAccessKey);
+        if (sessionToken) {
+          core.setSecret(sessionToken);
+          core.exportVariable('AWS_SESSION_TOKEN', sessionToken);
+        }
+        resolve();
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
 }
 
 exports.awsLogin = awsLogin;
